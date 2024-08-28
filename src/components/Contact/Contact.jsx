3566a1ca694e49905f5511c1e117cff24db5c681
II-777 <45958058+II-777@users.noreply.github.com>
2024-08-28 18:54:53 -0400

    update mockapi data model and Contact component

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import css from './Contact.module.css';
 import { FaUser } from 'react-icons/fa';
 import { ImPhone } from "react-icons/im";
 
-export default function Contact({ data: { id, name, phone }, onDelete }) {
+export default function Contact({ data: { id, name, number }, onDelete }) {
   return (
     <div className={css.container}>
       <div>
@@ -10,7 +10,7 @@ export default function Contact({ data: { id, name, phone }, onDelete }) {
           <FaUser className={css.icon} /> {name}
         </p>
         <p className={css.phone}>
-          <ImPhone className={css.icon} /> {phone}
+          <ImPhone className={css.icon} /> {number}
         </p>
       </div>
       <button className={css.btn} onClick={() => onDelete(id)}>
